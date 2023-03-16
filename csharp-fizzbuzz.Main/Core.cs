7d4ad5c031037e8b90730a2a5fd5ada8aecaee20
uerbzr <nigel.sibbert@gmail.com>
2023-03-16 17:25:18 +0000

    Updated Core/Extention..

--- csharp-fizzbuzz.Main/Core.cs
+++ csharp-fizzbuzz.Main/Core.cs
@@ -18,7 +18,7 @@ namespace csharp_fizzbuzz.Main
 
         public String[] fizzbuzz(String[] nums)
         {
-            throw new NotImplementedException();
+
 
             /* TODO: Implement a function that will look through a list of String representation of numbers and
                 change the values of said array based on the rules below
@@ -26,7 +26,21 @@ namespace csharp_fizzbuzz.Main
                 Where a number is a multiple of five (5, 10, etc) the number in the array should be replaced the string "Buzz".
                 Where a number is a multiple of both five AND three (15, 30, etc) the number in the array should be replaced the string "Fizzbuzz".
             * */
-
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if ((int.Parse(nums[i]) % 15) == 0)
+                {
+                    nums[i] = "Fizzbuzz";
+                }
+                else if ((int.Parse(nums[i]) % 3) == 0)
+                {
+                    nums[i] = "Fizz";
+                }
+                else if ((int.Parse(nums[i]) % 5) == 0)
+                {
+                    nums[i] = "Buzz";
+                }
+            }
 
 
             return nums;
--- csharp-fizzbuzz.Main/Extension.cs
+++ csharp-fizzbuzz.Main/Extension.cs
@@ -8,18 +8,35 @@ namespace csharp_fizzbuzz.Main
 {
     public class Extension
     {
-        public string[] fizzbuzz(int lower, int higher)
+        String[] nums;
+        public String[] fizzbuzz(int lower, int higher)
         {
-            throw new NotImplementedException();
             /* TODO: Implement a function that will create a list of strings containing the numbers from the lower number to the higher number
                 Where a number is a multiple of three (3, 6, 9, etc) the array should contain the string "Fizz" instead of a number.
                 Where a number is a multiple of five (5, 10, etc) the array should contain the string "Buzz" instead of a number.
                 Where a number is a multiple of both five AND three (15, 30, etc) the array should contain the string "Fizzbuzz" instead of a number.
             * */
-
-
-
-            return null;
+            this.nums = new String[higher - lower + 1];
+            for (int i = lower, index = 0; i <= this.nums.Length; i++, index++)
+            {
+                if ((i % 15) == 0)
+                {
+                    this.nums[index] = "Fizzbuzz";
+                }
+                else if ((i % 3) == 0)
+                {
+                    this.nums[index] = "Fizz";
+                }
+                else if ((i % 5) == 0)
+                {
+                    this.nums[index] = "Buzz";
+                }
+                else
+                {
+                    this.nums[index] = i.ToString();
+                }
+            }
+            return this.nums;
         }
     }
 }
